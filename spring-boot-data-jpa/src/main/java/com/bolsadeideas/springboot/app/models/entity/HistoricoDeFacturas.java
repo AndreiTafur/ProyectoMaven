01aab28f99ac72c601aaeff86adf6b09a8015b7e
lucho9225 <lfrodriguez9225@gmail.com>
2018-06-12 15:58:39 -0500

    v-1.0
    Creación de la clase HistoricoDeFacturas

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/HistoricoDeFacturas.java
@@ -0,0 +1,54 @@
+package com.bolsadeideas.springboot.app.models.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class HistoricoDeFacturas {
+	
+	private Factura factura;
+	
+	private String Observacion;
+	
+	@NotNull
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createAt;
+
+	public HistoricoDeFacturas(Factura factura) {
+		super();
+		this.factura = factura;
+	}
+
+	public Factura getFactura() {
+		return factura;
+	}
+
+	public void setFactura(Factura factura) {
+		this.factura = factura;
+	}
+
+	public String getObservacion() {
+		return Observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		Observacion = observacion;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	
+}

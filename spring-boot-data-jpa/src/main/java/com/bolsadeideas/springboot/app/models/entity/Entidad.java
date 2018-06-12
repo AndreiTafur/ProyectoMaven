0c34322adf6cdee69c729109836ef17591131949
SergioSepulveda <segcruz9419@gmail.com>
2018-06-12 15:41:02 -0500

    Actualizacion clase entidad

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Entidad.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Entidad.java
@@ -2,12 +2,14 @@ package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -35,6 +37,15 @@ public class Entidad implements Serializable {
 	
 	@NotEmpty
 	private int telefono;
+	
+	@ManyToMany(mappedBy="listaEmpleados")
+	private List<Empleado> listaempleados;
+	
+	@ManyToMany(mappedBy="listaClientes")
+	private List<Cliente> listaclientes;
+	
+	@ManyToMany(mappedBy="listaProductos")
+	private List<Producto> listaproductos;
 
 	public Long getId() {
 		return id;
@@ -67,7 +78,37 @@ public class Entidad implements Serializable {
 	public void setTelefono(int telefono) {
 		this.telefono = telefono;
 	}
-	
+
+	public List<Empleado> getListaempleados() {
+		return listaempleados;
+	}
+
+	public void setListaempleados(List<Empleado> listaempleados) {
+		this.listaempleados = listaempleados;
+	}
+
+	public List<Cliente> getListaclientes() {
+		return listaclientes;
+	}
+
+	public void setListaclientes(List<Cliente> listaclientes) {
+		this.listaclientes = listaclientes;
+	}
+
+	public List<Producto> getListaproductos() {
+		return listaproductos;
+	}
+
+	public void setListaproductos(List<Producto> listaproductos) {
+		this.listaproductos = listaproductos;
+	}
+
+	@Override
+	public String toString() {
+		return "Entidad [id=" + id + ", nombre=" + nombre + ", email=" + email + ", telefono=" + telefono
+				+ ", listaclientes=" + listaclientes + ", listaproductos=" + listaproductos + "]";
+	}
+
 	
 }
 
